3ab702119170f2d04599c15aec058a7775af6a59
Keshab sigdel <sigdelkeshab445@gmail.com>
2026-02-15 00:45:00 +0545

    server.js file

--- src/server.js
+++ src/server.js
@@ -1,17 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import http from "http";
+import axios from "axios";
 import {connectDB} from './config/mongodb.js';
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 
 dotenv.config();
-
 const app = express();
+app.use(cors({
+  origin: "http://localhost:5173", 
+  credentials: true,              
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 
 app.use(express.json());
 app.use(cookieParser());
