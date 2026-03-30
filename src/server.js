a9efa2df37b7d48a166eca1e231f09f5edee688a
keshab-DOT <sigdelkeshab445@gmail.com>
2026-03-30 23:31:26 +0545

    update frontend url in cors

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://healthhaul.netlify.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 }));
