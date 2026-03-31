0610cfec9f4c852ac0cdf987cf69a58a28108525
keshab-DOT <sigdelkeshab445@gmail.com>
2026-03-31 15:25:25 +0545

    fixed parameters issue

--- src/server.js
+++ src/server.js
@@ -62,7 +62,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
-app.options("*", cors());
+// app.options("*", cors());
 
 app.use(express.json());
 app.use(cookieParser());
